187105e1019ed69772e55e2262ce3f42e13dc4ff
andrijslmkh <andrijsalamakha@gmail.com>
2023-10-06 15:30:45 +0300

    update stats europa league

--- js/json_js/europa_league_json.js
+++ js/json_js/europa_league_json.js
@@ -1,426 +1,426 @@
 const europaLeagueJson = [
-	{
-		"group_name": "a",
-		"teams": [
-			{
-				"id": 28,
-				"name": "Вест Хем",
-				"alt": "west-ham",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 81,
-				"name": "Фрайбург",
-				"alt": "freigurg",
-				"position": 2,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 48,
-				"name": "Олімпіакос Пірей",
-				"alt": "olympiakos-piraeus",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 237,
-				"name": "Бачка-Топола",
-				"alt": "tsc-backa-topola",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			}
-		]
-	},
-	{
-		"group_name": "b",
-		"teams": [
-			{
-				"id": 83,
-				"name": "Брайтон",
-				"alt": "brighton",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 49,
-				"name": "Марсель",
-				"alt": "marseille",
-				"position": 2,
-				"stats": {
-					"win": 0,
-					"draw": 1,
-					"lose": 0,
-					"points": 1
-				}
-			},
-			{
-				"id": 19,
-				"name": "Аякс",
-				"alt": "ajax",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 1,
-					"lose": 0,
-					"points": 1
-				}
-			},
-			{
-				"id": 135,
-				"name": "АЕК",
-				"alt": "aek",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			}
-		]
-	},
-	{
-		"group_name": "c",
-		"teams": [
-			{
-				"id": 94,
-				"name": "Спарта Прага",
-				"alt": "sparta-praha",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 52,
-				"name": "Бетіс",
-				"alt": "betis",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 154,
-				"name": "Аріс",
-				"alt": "aris-tt-bank",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 29,
-				"name": "Рейнджерс",
-				"alt": "rangers",
-				"position": 2,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			}
-		]
-	},
-	{
-		"group_name": "d",
-		"teams": [
-			{
-				"id": 107,
-				"name": "Штурм Грац",
-				"alt": "sturm-graz",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 23,
-				"name": "Аталанта",
-				"alt": "atalanta",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 35,
-				"name": "Спортінг",
-				"alt": "sporting-lisabon",
-				"position": 2,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 162,
-				"name": "Ракув",
-				"alt": "rks-rakow-czestochowa",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			}
-		]
-	},
-	{
-		"group_name": "e",
-		"teams": [
-			{
-				"id": 5,
-				"name": "Ліверпуль",
-				"alt": "liverpool",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 46,
-				"name": "ЛАСК",
-				"alt": "lask-linz",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 72,
-				"name": "Юніон Сент-Жілуаз",
-				"alt": "royale-union-sg",
-				"position": 2,
-				"stats": {
-					"win": 0,
-					"draw": 1,
-					"lose": 0,
-					"points": 1
-				}
-			},
-			{
-				"id": 130,
-				"name": "Тулуза",
-				"alt": "touluse",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 1,
-					"lose": 0,
-					"points": 1
-				}
-			}
-		]
-	},
-	{
-		"group_name": "f",
-		"teams": [
-			{
-				"id": 47,
-				"name": "Ренн",
-				"alt": "rennes",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 17,
-				"name": "Вільярреал",
-				"alt": "villarreal",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 262,
-				"name": "Панатінаїкос",
-				"alt": "panathinaikos",
-				"position": 2,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 91,
-				"name": "Маккабі Хайфа",
-				"alt": "maccabi-haifa",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			}
-		]
-	},
-	{
-		"group_name": "g",
-		"teams": [
-			{
-				"id": 40,
-				"name": "Славія Прага",
-				"alt": "slavia-praha",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 10,
-				"name": "Рома",
-				"alt": "roma",
-				"position": 2,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 209,
-				"name": "Серветт",
-				"alt": "servette",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 77,
-				"name": "Шериф",
-				"alt": "sheriff-tiraspol",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			}
-		]
-	},
-	{
-		"group_name": "h",
-		"teams": [
-			{
-				"id": 210,
-				"name": "Хеккен",
-				"alt": "hacken",
-				"position": 4,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 20,
-				"name": "Баєр Леверкузен",
-				"alt": "bayer-leverkusen",
-				"position": 1,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			},
-			{
-				"id": 63,
-				"name": "Мольде",
-				"alt": "molde",
-				"position": 3,
-				"stats": {
-					"win": 0,
-					"draw": 0,
-					"lose": 1,
-					"points": 0
-				}
-			},
-			{
-				"id": 61,
-				"name": "Карабах",
-				"alt": "garabag-agdan",
-				"position": 2,
-				"stats": {
-					"win": 1,
-					"draw": 0,
-					"lose": 0,
-					"points": 3
-				}
-			}
-		]
-	}
+  {
+    group_name: "a",
+    teams: [
+      {
+        id: 28,
+        name: "Вест Хем",
+        alt: "west-ham",
+        position: 1,
+        stats: {
+          win: 2,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+      {
+        id: 81,
+        name: "Фрайбург",
+        alt: "freigurg",
+        position: 2,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 48,
+        name: "Олімпіакос Пірей",
+        alt: "olympiakos-piraeus",
+        position: 3,
+        stats: {
+          win: 0,
+          draw: 1,
+          lose: 1,
+          points: 1,
+        },
+      },
+      {
+        id: 237,
+        name: "Бачка-Топола",
+        alt: "tsc-backa-topola",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 1,
+          lose: 1,
+          points: 1,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "b",
+    teams: [
+      {
+        id: 83,
+        name: "Брайтон",
+        alt: "brighton",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 1,
+          lose: 1,
+          points: 1,
+        },
+      },
+      {
+        id: 49,
+        name: "Марсель",
+        alt: "marseille",
+        position: 2,
+        stats: {
+          win: 0,
+          draw: 2,
+          lose: 0,
+          points: 2,
+        },
+      },
+      {
+        id: 19,
+        name: "Аякс",
+        alt: "ajax",
+        position: 3,
+        stats: {
+          win: 0,
+          draw: 2,
+          lose: 0,
+          points: 2,
+        },
+      },
+      {
+        id: 135,
+        name: "АЕК",
+        alt: "aek",
+        position: 1,
+        stats: {
+          win: 1,
+          draw: 1,
+          lose: 0,
+          points: 4,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "c",
+    teams: [
+      {
+        id: 94,
+        name: "Спарта Прага",
+        alt: "sparta-praha",
+        position: 1,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 52,
+        name: "Бетіс",
+        alt: "betis",
+        position: 3,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 154,
+        name: "Аріс",
+        alt: "aris-tt-bank",
+        position: 2,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 29,
+        name: "Рейнджерс",
+        alt: "rangers",
+        position: 4,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "d",
+    teams: [
+      {
+        id: 107,
+        name: "Штурм Грац",
+        alt: "sturm-graz",
+        position: 3,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 23,
+        name: "Аталанта",
+        alt: "atalanta",
+        position: 1,
+        stats: {
+          win: 2,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+      {
+        id: 35,
+        name: "Спортінг",
+        alt: "sporting-lisabon",
+        position: 2,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 162,
+        name: "Ракув",
+        alt: "rks-rakow-czestochowa",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 0,
+          lose: 2,
+          points: 0,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "e",
+    teams: [
+      {
+        id: 5,
+        name: "Ліверпуль",
+        alt: "liverpool",
+        position: 1,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+      {
+        id: 46,
+        name: "ЛАСК",
+        alt: "lask-linz",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 0,
+          lose: 2,
+          points: 0,
+        },
+      },
+      {
+        id: 72,
+        name: "Юніон Сент-Жілуаз",
+        alt: "royale-union-sg",
+        position: 3,
+        stats: {
+          win: 0,
+          draw: 1,
+          lose: 1,
+          points: 1,
+        },
+      },
+      {
+        id: 130,
+        name: "Тулуза",
+        alt: "touluse",
+        position: 2,
+        stats: {
+          win: 1,
+          draw: 1,
+          lose: 0,
+          points: 4,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "f",
+    teams: [
+      {
+        id: 47,
+        name: "Ренн",
+        alt: "rennes",
+        position: 3,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 17,
+        name: "Вільярреал",
+        alt: "villarreal",
+        position: 2,
+        stats: {
+          win: 1,
+          draw: 0,
+          lose: 1,
+          points: 3,
+        },
+      },
+      {
+        id: 262,
+        name: "Панатінаїкос",
+        alt: "panathinaikos",
+        position: 1,
+        stats: {
+          win: 1,
+          draw: 1,
+          lose: 0,
+          points: 4,
+        },
+      },
+      {
+        id: 91,
+        name: "Маккабі Хайфа",
+        alt: "maccabi-haifa",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 1,
+          lose: 1,
+          points: 1,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "g",
+    teams: [
+      {
+        id: 40,
+        name: "Славія Прага",
+        alt: "slavia-praha",
+        position: 1,
+        stats: {
+          win: 2,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+      {
+        id: 10,
+        name: "Рома",
+        alt: "roma",
+        position: 2,
+        stats: {
+          win: 2,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+      {
+        id: 209,
+        name: "Серветт",
+        alt: "servette",
+        position: 3,
+        stats: {
+          win: 0,
+          draw: 0,
+          lose: 2,
+          points: 0,
+        },
+      },
+      {
+        id: 77,
+        name: "Шериф",
+        alt: "sheriff-tiraspol",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 0,
+          lose: 2,
+          points: 0,
+        },
+      },
+    ],
+  },
+  {
+    group_name: "h",
+    teams: [
+      {
+        id: 210,
+        name: "Хеккен",
+        alt: "hacken",
+        position: 4,
+        stats: {
+          win: 0,
+          draw: 0,
+          lose: 2,
+          points: 0,
+        },
+      },
+      {
+        id: 20,
+        name: "Баєр Леверкузен",
+        alt: "bayer-leverkusen",
+        position: 1,
+        stats: {
+          win: 2,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+      {
+        id: 63,
+        name: "Мольде",
+        alt: "molde",
+        position: 3,
+        stats: {
+          win: 0,
+          draw: 0,
+          lose: 2,
+          points: 0,
+        },
+      },
+      {
+        id: 61,
+        name: "Карабах",
+        alt: "garabag-agdan",
+        position: 2,
+        stats: {
+          win: 2,
+          draw: 0,
+          lose: 0,
+          points: 6,
+        },
+      },
+    ],
+  },
 ];
